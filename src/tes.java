dcb093c4585067fa93603259e7e6d5db4303a514
sumertayoga <sumertayg@gmail.com>
2022-01-20 22:33:10 +0700

    coba coba awal

---
+++ src/tes.java
@@ -0,0 +1,11 @@
+
+public class tes {
+    static void plus(int y, int x) {
+        System.out.println(x+y);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hallo, Brow!");
+        plus(1,5);
+    }
+    
+}
