469cc3529c0b72c4947db3be78b1c22f686bcf4f
sumertayoga <sumertayg@gmail.com>
2022-01-23 10:06:02 +0700

    Matriks puzel dan kata target

--- src/tes.java
+++ src/tes.java
@@ -3,48 +3,45 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class tes {
-    static void plus(int y, int x) {
-        System.out.println(x+y);
-    }
     public static void main(String[] args) throws IOException{
-        System.out.println("Hallo, Brow!");
-        plus(1,5);
-        int[][] matrix = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-        System.out.println(matrix[1][2]);
-
-        // Mencari banyak kolom dan baris
-        //File obj = new File("src/teks.txt");
-        File obj = new File("../test/teks.txt");
-        Scanner myReader = new Scanner(obj);
-        int baris = 1;
-        String col = myReader.nextLine();
-        while(myReader.has){
+        Scanner obj = new Scanner(new File("../test/teks.txt"));
+        int baris = 0;
+        int kolom;
+        String line = obj.nextLine();
+        String[] col = line.split(" ");
+        kolom = col.length;
+        while(!line.isEmpty()){
             baris++;
-            col = myReader.nextLine();
+            line = obj.nextLine();
         }
-        String[] panjang = col.split(" ");
-        int kolom = panjang.length;
-        myReader.close();
+        int listLength = 0;
+        while(obj.hasNextLine()){
+            listLength++;
+            line = obj.nextLine();
+        }
+        obj.close();
 
-        System.out.println(baris);
-
-        myReader = new Scanner(obj);
+        
+        obj = new Scanner(new File("../test/teks.txt"));
         String[][] matriks = new String[baris][kolom];
         for(int i = 0; i < baris; i++){
             for(int j = 0; j < kolom; j++){
-                if(myReader.hasNext()){
-                    matriks[i][j] = myReader.next();
-                    System.out.print(matriks[i][j]); //Ngetes print matriksnya
+                if(obj.hasNext()){
+                    matriks[i][j] = obj.next();
+                    System.out.print(matriks[i][j]);
                 }
             }
-            System.out.println(); // Ngetes print matriksnya
+            System.out.println();
         }
-        myReader.close();
-
+        line = obj.nextLine();
+        line = obj.nextLine();
+        String[] listWord = new String[listLength];
+        for(int i = 0; i < listLength; i++){
+            listWord[i] = obj.nextLine();
+            System.out.println(listWord[i]);
+        }
+        obj.close();
+        
 
     }
     
